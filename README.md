590a25b523daf9a155264c9a3cb87c40e0eec0e0
Konstantin Belykh <k@belykh.su>
2024-09-11 19:00:23 +0400

    Added ability to exclude IP address, script overhaul and simplification, remove unnecessary options

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ python3 -m pip install netaddr
 
  1. Create configuration file named `config.json` and fill it based on `config.json.example`. For additional information about config see below.
  2. Install bird and configure `bird.conf` based on `bird.conf.example` (additional information about confuration parameters is below)
- 3. Install some VPN software on VPN and configure client on your router. For example you can see https://github.com/hwdsl2/setup-ipsec-vpn or https://github.com/angristan/wireguard-install 
+ 3. Install some VPN software on VPN and configure client on your router. For example you can see https://github.com/hwdsl2/setup-ipsec-vpn or https://github.com/angristan/wireguard-install
  4. Configure BGP on your client based on your `bird.conf`
  My example on Mikrotik RouterOS 7:
 ````
@@ -31,44 +31,43 @@ add chain=static_bgp disabled=no rule="set gw-interface l2tp_russia; set distanc
 
  1. `Sources` - list of sources to get data from. You probably want to use all of them.
  2. `Countries` - list of countries to get data for. Possbile values are listed in section `List of possible country codes` of this document. Example: `"Countries": ["RU", "UA", "BY", "KZ"]`
- 3. `EnableIPv4` - enable or disable gathering IPv4 data. Possible values: `true` or `false`  
-    3.1. `AppendIPv4` - list of additional IPv4 networks to append to list. Example: `"AppendIPv4": ["192.168.0.0/24", "192.168.1.0/24"]`  
-    3.2. `OutputFileipv4` - path to file where to save IPv4 networks. Example: `"/etc/bird/list.txt"`  
- 4. `EnableIPv6` - enable or disable gathering IPv6 data. Possible values: `true` or `false`  
-    4.1. `AppendIPv6` - list of additional IPv6 networks to append to list. Example: `"AppendIPv6": ["2001:db8::/32", "2001:db8:1::/48"]`  
-    4.2. `OutputFileipv6` - path to file where to save IPv6 networks. Example: `"/etc/bird/list6.txt"`  
- 5. `SummarizeNetworks` - enable or disable summarizing adjasent networks (eg 10.0.0.0/24 + 10.0.1.0/24 = 10.0.0.0/23). Possible values: `true` or `false`
- 6. `SummarizeOutput` - enable saving both IPv4 and IPv6 networks in one file. Possible values: `true` or `false`. 
- 7. `SummarizeOutputFile` - path to file where to save summarized networks. Example: `"/etc/bird/listall.txt"`
- 8. `OutputFormat` - format of line in output. Example: `"route {0} reject;"`, where `{0}` is replaced with network.
+ 3. `EnableIPv4` - enable or disable gathering IPv4 data. Possible values: `true` or `false`
+    3.1. `AppendIPv4` - list of additional IPv4 networks to append to list. Example: `"AppendIPv4": ["192.168.0.0/24", "192.168.1.0/24"]`
+    3.2. `ExcludeIPv4` - same as Append, but exclude
+    3.3. `OutputFileipv4` - path to file where to save IPv4 networks. Example: `"/etc/bird/list.txt"`
+ 4. `EnableIPv6` - enable or disable gathering IPv6 data. Possible values: `true` or `false`
+    4.1. `AppendIPv6` - list of additional IPv6 networks to append to list. Example: `"AppendIPv6": ["2001:db8::/32", "2001:db8:1::/48"]`
+    4.2. `ExcludeIPv4` - same as Append, but exclude
+    4.3. `OutputFileipv6` - path to file where to save IPv6 networks. Example: `"/etc/bird/list6.txt"`
+ 5. `OutputFormat` - format of line in output. Example: `"route {0} reject;"`, where `{0}` is replaced with network.
 
 ## List of possible country codes
 
 Below is list of contry codes that can be used in `Countries` section of `config.json`:
 ```
-"AD", "AE", "AF", "AG", "AI", "AL", "AM", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AW", "AX", "AZ", 
-"BA", "BB", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BL", "BM", "BN", "BO", "BQ", "BR", "BS", "BT", "BW", "BY", "BZ", 
-"CA", "CD", "CF", "CG", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CU", "CV", "CW", "CY", "CZ", 
-"DE", "DJ", "DK", "DM", "DO", "DZ", 
-"EC", "EE", "EG", "ER", "ES", "ET", "EU", 
-"FI", "FJ", "FK", "FM", "FO", "FR", 
-"GA", "GB", "GD", "GE", "GF", "GG", "GH", "GI", "GL", "GM", "GN", "GP", "GQ", "GR", "GT", "GU", "GW", "GY", 
-"HK", "HN", "HR", "HT", "HU", 
-"ID", "IE", "IL", "IM", "IN", "IO", "IQ", "IR", "IS", "IT", 
-"JE", "JM", "JO", "JP", 
-"KE", "KG", "KH", "KI", "KM", "KN", "KP", "KR", "KW", "KY", "KZ", 
-"LA", "LB", "LC", "LI", "LK", "LR", "LS", "LT", "LU", "LV", "LY", 
-"MA", "MC", "MD", "ME", "MF", "MG", "MH", "MK", "ML", "MM", "MN", "MO", "MP", "MQ", "MR", "MS", "MT", "MU", "MV", "MW", "MX", "MY", "MZ", 
-"NA", "NC", "NE", "NF", "NG", "NI", "NL", "NO", "NP", "NR", "NU", "NZ", 
-"OM", 
-"PA", "PE", "PF", "PG", "PH", "PK", "PL", "PM", "PR", "PS", "PT", "PW", "PY", 
-"QA", 
-"RE", "RO", "RS", "RU", "RW", 
-"SA", "SB", "SC", "SD", "SE", "SG", "SI", "SK", "SL", "SM", "SN", "SO", "SR", "SS", "ST", "SV", "SX", "SY", "SZ", 
-"TC", "TD", "TF", "TG", "TH", "TJ", "TK", "TL", "TM", "TN", "TO", "TR", "TT", "TV", "TW", "TZ", 
-"UA", "UG", "US", "UY", "UZ", 
-"VA", "VC", "VE", "VG", "VI", "VN", "VU", 
-"WF", "WS", 
-"YE", "YT", 
+"AD", "AE", "AF", "AG", "AI", "AL", "AM", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AW", "AX", "AZ",
+"BA", "BB", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BL", "BM", "BN", "BO", "BQ", "BR", "BS", "BT", "BW", "BY", "BZ",
+"CA", "CD", "CF", "CG", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CU", "CV", "CW", "CY", "CZ",
+"DE", "DJ", "DK", "DM", "DO", "DZ",
+"EC", "EE", "EG", "ER", "ES", "ET", "EU",
+"FI", "FJ", "FK", "FM", "FO", "FR",
+"GA", "GB", "GD", "GE", "GF", "GG", "GH", "GI", "GL", "GM", "GN", "GP", "GQ", "GR", "GT", "GU", "GW", "GY",
+"HK", "HN", "HR", "HT", "HU",
+"ID", "IE", "IL", "IM", "IN", "IO", "IQ", "IR", "IS", "IT",
+"JE", "JM", "JO", "JP",
+"KE", "KG", "KH", "KI", "KM", "KN", "KP", "KR", "KW", "KY", "KZ",
+"LA", "LB", "LC", "LI", "LK", "LR", "LS", "LT", "LU", "LV", "LY",
+"MA", "MC", "MD", "ME", "MF", "MG", "MH", "MK", "ML", "MM", "MN", "MO", "MP", "MQ", "MR", "MS", "MT", "MU", "MV", "MW", "MX", "MY", "MZ",
+"NA", "NC", "NE", "NF", "NG", "NI", "NL", "NO", "NP", "NR", "NU", "NZ",
+"OM",
+"PA", "PE", "PF", "PG", "PH", "PK", "PL", "PM", "PR", "PS", "PT", "PW", "PY",
+"QA",
+"RE", "RO", "RS", "RU", "RW",
+"SA", "SB", "SC", "SD", "SE", "SG", "SI", "SK", "SL", "SM", "SN", "SO", "SR", "SS", "ST", "SV", "SX", "SY", "SZ",
+"TC", "TD", "TF", "TG", "TH", "TJ", "TK", "TL", "TM", "TN", "TO", "TR", "TT", "TV", "TW", "TZ",
+"UA", "UG", "US", "UY", "UZ",
+"VA", "VC", "VE", "VG", "VI", "VN", "VU",
+"WF", "WS",
+"YE", "YT",
 "ZA", "ZM", "ZW"
 ```
--- createtable.py
+++ createtable.py
@@ -3,45 +3,88 @@
 """
 Author - Kostya Belykh k@belykh.su
 Language - Python 3.8+
-This script is designed to parse data from country network addresses and 
+This script is designed to parse data from country network addresses and
 generate configuration file for bind to distribute as BGP table. For additional
 information please refer to README.md file.
 """
 
-from optparse import OptionParser
-from netaddr import cidr_merge
-import logging
-import time
-import os
 import json
-import sys
-import urllib.request
-import ipaddress
+import logging
 import math
+import os
+import sys
+import time
+import urllib.request
+from ipaddress import ip_network
+from optparse import OptionParser
 
-start       = time.time()
-logger      = logging.getLogger()
+from netaddr import cidr_merge
+
+start = time.time()
+logger = logging.getLogger()
+
+
+def netmaskcidr(num_hosts):
+    # Return cidr notation of netmask based on number of hosts it contains
+    required_bits = math.ceil(math.log2(int(num_hosts)))
+    network_bits = 32 - required_bits
+    return f"{network_bits}"
+
+
+def updatesubnet(networks, subnets_to_remove, subnets_to_add):
+    networks = cidr_merge(networks)
+    returnnetworks = set()
+    for network in networks:
+        if subnets_to_remove:
+            for subnet in subnets_to_remove:
+                if ip_network(subnet, strict=False).overlaps(
+                    ip_network(network, strict=False)
+                ):
+                    resultnets = list(
+                        ip_network(network, strict=False).address_exclude(
+                            ip_network(subnet, strict=False)
+                        )
+                    )
+                    for net in resultnets:
+                        returnnetworks.add(str(net))
+                else:
+                    returnnetworks.add(network)
+        else:
+            returnnetworks.add(network)
+    for subnet in subnets_to_add:
+        returnnetworks.add(subnet)
+    # Correct data in case some registrars have wrong info about network
+    corrected_networks = set()
+    for network in returnnetworks:
+        n = ip_network(network, strict=False)
+        corrected_networks.add(str(f"{n.network_address}/{n.prefixlen}"))
+    return corrected_networks
+
 
 def parseCommandOptions():
 
     # Parsing input parameters
-    p = OptionParser(add_help_option=False,
-                     usage = '%prog [options]'
+    p = OptionParser(add_help_option=False, usage="%prog [options]")
+    p.add_option(
+        "-h",
+        "--help",
+        action="store_true",
+        dest="help",
+        help="Show this message and exit",
     )
-    p.add_option('-h', '--help',
-                 action = 'store_true',
-                 dest = 'help',
-                 help = 'Show this message and exit'
+    p.add_option(
+        "-l",
+        "--logging",
+        dest="enablelogging",
+        help="Enable logging to STDOUT",
+        action="store_true",
     )
-    p.add_option('-l', '--logging',
-                 dest = 'enablelogging',
-                 help = 'Enable logging to STDOUT',
-                 action='store_true'
-    )
-    p.add_option('-c', '--config',
-                 dest = 'configfile',
-                 help = 'Configuration file to use, default is ./config.json',
-                 default = os.path.join(sys.path[0],'config.json')
+    p.add_option(
+        "-c",
+        "--config",
+        dest="configfile",
+        help="Configuration file to use, default is ./config.json",
+        default=os.path.join(sys.path[0], "config.json"),
     )
 
     (options, args) = p.parse_args()
@@ -52,8 +95,8 @@ def parseCommandOptions():
 
     ## Setup logging
     if options.enablelogging:
-        loglevel = 'INFO'
-        logFormatStr = '%(asctime)s - %(levelname)-8s - %(message)s'
+        loglevel = "INFO"
+        logFormatStr = "%(asctime)s - %(levelname)-8s - %(message)s"
         logger.setLevel(loglevel)
         chandler = logging.StreamHandler()
         formatter = logging.Formatter(logFormatStr)
@@ -62,185 +105,99 @@ def parseCommandOptions():
 
     main(options.configfile)
 
-def main(configfile):
-    logging.info('************************************************************')
-    logging.info('Script started')
 
-    logging.info('Importing configuration file')
-    try: 
-        with open(configfile, encoding='utf-8') as f:
+def main(configfile):
+    logging.info("************************************************************")
+    logging.info("Script started")
+
+    logging.info("Importing configuration file")
+    try:
+        with open(configfile, encoding="utf-8") as f:
             config = json.load(f)
     except FileNotFoundError:
-        sys.exit('Configuration file not found, exiting')
+        sys.exit("Configuration file not found, exiting")
 
-    logging.info('Current configuration:')
-    logging.info('  Sources:')
-    for s in config['Sources']:
-        logging.info('    Name: {}, Address: {}'.format(
-            s['Name'], s['Address']
-        ))
-    logging.info('  Countries: {}'.format(config['Countries']))
-    logging.info('  Enable IPv4: {}'.format(bool(config['EnableIPv4'])))
-    if bool(config['EnableIPv4']) == True:
-        logging.info('    IPv4 networks to be appended: {}'.format(
-            config['AppendIPv4']
-        ))
-        logging.info('    Output file for IPv4 networks: {}'.format(
-            config['OutputFileipv4']
-        ))
-    logging.info('  Enable IPv6: {}'.format(bool(config['EnableIPv6'])))
-    if bool(config['EnableIPv6']) == True:
-        logging.info('    IPv6 networks to be appended: {}'.format(
-            config['AppendIPv6']
-        ))
-        logging.info('    Output file for ipv6 networks: {}'.format(
-            config['OutputFileipv6']
-        ))
-    logging.info('  Summarize networks: {}'.format(
-        bool(config['SummarizeNetworks'])
-    ))
-    logging.info('  Summarize output file: {}'.format(
-        config['SummarizeOutput']
-    ))
-    if bool(config['SummarizeOutput']) == True:
-        logging.info('    Output file for summarized networks: {}'.format(
-            config['SummarizeOutputFile']
-        ))
-    logging.info('  Output format: {}'.format(config['OutputFormat']))
+    logging.info("Current configuration:")
 
-    logging.info('Retrieving data from sources')
+    logging.info("  Sources:")
+    for s in config["Sources"]:
+        logging.info(f'    Name: {s["Name"]}, Address: {s["Address"]}')
+    logging.info(f'  Countries: {config["Countries"]}')
+
+    logging.info(f'  Enable IPv4: {bool(config["EnableIPv4"])}')
+    if bool(config["EnableIPv4"]) == True:
+        logging.info(f'    IPv4 networks to be appended: {config["AppendIPv4"]}')
+        logging.info(f'    IPv4 networks to be excluded: {config["ExcludeIPv4"]}')
+        logging.info(f'    Output file for IPv4 networks: {config["OutputFileipv4"]}')
+
+    logging.info(f'  Enable IPv6: {bool(config["EnableIPv6"])}')
+    if bool(config["EnableIPv6"]) == True:
+        logging.info(f'    IPv6 networks to be appended: {config["AppendIPv6"]}')
+        logging.info(f'    IPv6 networks to be excluded: {config["ExcludeIPv6"]}')
+        logging.info(f'    Output file for ipv6 networks: {config["OutputFileipv6"]}')
+
+    logging.info(f'  Output format: {config["OutputFormat"]}')
+
+    logging.info("Retrieving data from sources")
     data = {}
-    for s in config['Sources']:
-        logging.info('  Retreiwing data from {}'.format(s['Name']))
+    for s in config["Sources"]:
+        logging.info(f'  Retreiwing data from {s["Name"]}')
         try:
-            with urllib.request.urlopen(s['Address']) as f:
-                data[s['Name']] = f.read().decode('utf-8').splitlines()
+            with urllib.request.urlopen(s["Address"]) as f:
+                data[s["Name"]] = f.read().decode("utf-8").splitlines()
         except urllib.error.URLError as e:
-            logging.error('  Unable to retrieve data from {}: {}'.format(
-                s['Name'],
-                e.reason
-            ))
-            sys.exit('Unable to retrieve data from {}: {}'.format(
-                s['Name'],
-                e.reason
-            ))
-    
-    logging.info('Generating report')
-    reportipv4 = []
-    reportipv6 = []
-    reportsum = []
-    for s in config['Sources']:
-        logging.info('  Processing data from {}'.format(s['Name']))
-        for line in data[s['Name']]:
+            logging.error(f'  Unable to retrieve data from {s["Name"]}: {e.reason}')
+            sys.exit(f'Unable to retrieve data from {s["Name"]}: {e.reason}')
+
+    logging.info("Generating report")
+    reportipv4 = set()
+    reportipv6 = set()
+    for s in config["Sources"]:
+        logging.info(f'  Processing data from {s["Name"]}')
+        for line in data[s["Name"]]:
             try:
-                l = line.split('|')
-                if l[1] in config['Countries']:
-                    if l[2] == 'ipv4' and bool(config['EnableIPv4']) == True:
-                        network = str(
-                            ipaddress.ip_network(
-                                str('{}/{}'.format(
-                                    l[3],
-                                    str(int(32-math.log(int(l[4]),2)))
-                                )), 
-                                strict=False
-                            )
-                        )
-                        if network not in reportipv4:
-                            reportipv4.append(network)
-                    if l[2] == 'ipv6' and bool(config['EnableIPv6']) == True:
-                        network = str(
-                            ipaddress.ip_network(
-                                str('{}/{}'.format(
-                                    l[3],
-                                    l[4]
-                                )), 
-                                strict=False
-                            )
-                        )
-                        if network not in reportipv6:
-                            reportipv6.append(network)
-                    if (
-                        l[2] == 'ipv4' or
-                        l[2] == 'ipv6'
-                    ) and bool(config['SummarizeOutput']) == True:
-                        if l[2] == 'ipv4':
-                            netmask = str(int(32-math.log(int(l[4]),2)))
-                        elif l[2] == 'ipv6':
-                            netmask = l[4]
-                        else:
-                            pass
-                        network = str(
-                            ipaddress.ip_network(
-                                str('{}/{}'.format(
-                                    l[3],
-                                    netmask
-                                )), 
-                                strict=False
-                            )
-                        )
-                        if network not in reportsum:
-                            reportsum.append(network)
+                l = line.split("|")
+                if l[1] in config["Countries"]:
+                    if l[2] == "ipv4" and bool(config["EnableIPv4"]) == True:
+                        reportipv4.add(str(f"{l[3]}/{netmaskcidr(l[4])}"))
+                    if l[2] == "ipv6" and bool(config["EnableIPv6"]) == True:
+                        reportipv6.add(str(f"{l[3]}/{l[4]}"))
             except IndexError:
-                # Sometimes lines do not have separators 
+                # Sometimes lines do not have separators
                 pass
-    if bool(config['EnableIPv4']) == True:
-        for network in config['AppendIPv4']:
-            if network not in reportipv4:
-                reportipv4.append(network)
-    if bool(config['EnableIPv6']) == True:
-        for network in config['AppendIPv6']:
-            if network not in reportipv6:
-                reportipv6.append(network)
-    logging.info('  Report generation complete')
-    if bool(config['EnableIPv4']) == True:
-        logging.info('    IPv4 networks: {}'.format(len(reportipv4)))
-    if bool(config['EnableIPv6']) == True:
-        logging.info('    IPv6 networks: {}'.format(len(reportipv6)))
-    if bool(config['SummarizeOutput']) == True:
-        logging.info('    Sum of all networks: {}'.format(len(reportsum)))
-
-    if bool(config['SummarizeNetworks']) == True:
-        logging.info('Summarizing networks')
-        if bool(config['EnableIPv4']) == True:
-            reportipv4 = cidr_merge(reportipv4)
-            logging.info('  IPv4 networks: {}'.format(len(reportipv4)))
-        if bool(config['EnableIPv6']) == True:
-            reportipv6 = cidr_merge(reportipv6)
-            logging.info('  IPv6 networks: {}'.format(len(reportipv6)))
-        if bool(config['SummarizeOutput']) == True:
-            reportsum = cidr_merge(reportsum)
-            logging.info('  Sum of all networks: {}'.format(len(reportsum)))
-    
-    logging.info('Saving report')
-    if bool(config['EnableIPv4']) == True:
-        logging.info('  Saving IPv4 report to {}'.format(
-            config['OutputFileipv4']
-        ))
-        with open(config['OutputFileipv4'], 'w') as file:
-            for line in reportipv4:
-                file.write(config['OutputFormat'].format(str(line))+'\n')
-            file.close
-    if bool(config['EnableIPv6']) == True:
-        logging.info('  Saving IPv6 report to {}'.format(
-            config['OutputFileipv6']
-        ))
-        with open(config['OutputFileipv6'], 'w') as file:
-            for line in reportipv6:
-                file.write(config['OutputFormat'].format(str(line))+'\n')
-            file.close
-    if bool(config['SummarizeOutput']) == True:
-        logging.info('  Saving summarized report to {}'.format(
-            config['SummarizeOutputFile']
-        ))
-        with open(config['SummarizeOutputFile'], 'w') as file:
-            for line in reportsum:
-                file.write(config['OutputFormat'].format(str(line))+'\n')
-            file.close
-
-    logging.info('Script finished. Runtime: {} seconds.'.format(
-        str(round(time.time() - start, 3))
+    if bool(config["EnableIPv4"]) == True:
+        reportipv4 = updatesubnet(
+            reportipv4, config["ExcludeIPv4"], config["AppendIPv4"]
         )
+    if bool(config["EnableIPv6"]) == True:
+        reportipv6 = updatesubnet(
+            reportipv6, config["ExcludeIPv6"], config["AppendIPv6"]
+        )
+
+    logging.info("  Report generation complete")
+    if bool(config["EnableIPv4"]) == True:
+        logging.info(f"    IPv4 networks: {len(reportipv4)}")
+    if bool(config["EnableIPv6"]) == True:
+        logging.info(f"    IPv6 networks: {len(reportipv6)}")
+
+    logging.info("Saving report")
+    if bool(config["EnableIPv4"]) == True:
+        logging.info(f'  Saving IPv4 report to {config["OutputFileipv4"]}')
+        with open(config["OutputFileipv4"], "w") as file:
+            for line in reportipv4:
+                file.write(config["OutputFormat"].format(str(line)) + "\n")
+            file.close
+    if bool(config["EnableIPv6"]) == True:
+        logging.info(f'  Saving IPv6 report to {config["OutputFileipv6"]}')
+        with open(config["OutputFileipv6"], "w") as file:
+            for line in reportipv6:
+                file.write(config["OutputFormat"].format(str(line)) + "\n")
+            file.close
+
+    logging.info(
+        f"Script finished. Runtime: {str(round(time.time() - start, 3))} seconds."
     )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parseCommandOptions()
